4484ac4f52c74214ea780d58fad78333e3395943
mauserne <52377363+mauserne@users.noreply.github.com>
2022-02-15 11:37:05 +0900

    Create [ETC]Adventures-Guild.py
    모험가 길드 그리디 문제

---
+++ Greedy/[ETC]Adventures-Guild.py
@@ -0,0 +1,16 @@
+"""
+모험가 길드
+https://freedeveloper.tistory.com/367?category=888096
+"""
+
+a = int(input())
+list = list(map(int,input().split()))
+list.sort()
+x = set(list)   # 중복 제거위해 list를 집합으로 만들어 변수 x에 선언
+groups=0        # 그룹 갯수 
+
+for i in x:
+    groups += list.count(i) // i    # 자기 공포도만큼 그룹을 구성하기 위해
+                                    # list에 공포도 같은 사람 숫자를 공포도로 나누고 그룹갯수 올림
+
+print(groups)
