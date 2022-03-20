8cc0eb5191cd5febfb702ae4a82cd074c965304b
mauserne <52377363+mauserne@users.noreply.github.com>
2022-03-20 23:52:15 +0900

    Update NOTE.py

--- NOTE.py
+++ NOTE.py
@@ -1,30 +1,40 @@
-import sys
+from collections import deque
 
 
+n, m, v = map(int, input().split())
 
-n, m = map(int,input().split())
+graph = [[]for _ in range(n+1)]
+visited = [False] * (n+1)
 
-visited = [False] * n
-graph = [[]for i in range(n)]
 
-for _ in range(m):
-    a, b = map(int,sys.stdin.readline().rstrip().split())
+for i in range(m):
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def dfs(index, number):
-    if number == 4:
-        print(1)
-        exit()
-    for i in graph[index]:
+for i in range(1,n+1):
+    graph[i].sort()
+
+def dfs(x):
+    visited[x] = True
+    print(x, end = ' ')
+    for i in graph[x]:
         if not visited[i]:
-            visited[i] = True
-            dfs(i, number+1)
-            visited[i] = False
+            dfs(i)
 
-for i in range(n):
-    visited[i] = True
-    dfs(i, 0)
-    visited[i] = False
+def bfs(x):
+    visited[x] = True
+    queue = deque([x])
+    while queue:
+        x = queue.popleft()
+        print(x, end=' ')
+        for i in graph[x]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True  
 
-print(0)
+dfs(v)
+
+visited = [False] * (n+1)
+print()
+bfs(v)
