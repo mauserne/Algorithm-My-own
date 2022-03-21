96fb80b8e147a1f75bc759cce544f00cb44bdb45
mauserne <52377363+mauserne@users.noreply.github.com>
2022-03-21 23:56:29 +0900

    Create boj2178.py

---
+++ DFS-BFS/boj2178.py
@@ -0,0 +1,40 @@
+#BFS
+"""
+미로 탐색
+https://www.acmicpc.net/problem/2178
+"""
+from collections import deque
+
+
+
+
+n, m = map(int, input().split())
+
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx<0 or nx>=n or ny<0 or ny>=m: 
+                continue
+            elif matrix[nx][ny] == 1:
+                matrix[nx][ny] = matrix[x][y] + 1
+                queue.append((nx,ny))
+    
+    return matrix[n-1][m-1]
+
+
+matrix = []
+for i in range(n):
+    matrix.append(list(map(int , input())))
+
+print(bfs(0,0))
