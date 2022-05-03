dde219bad160fe1e887b06ad5753f8ae4f35a512
mauserne <52377363+mauserne@users.noreply.github.com>
2022-05-03 23:56:11 +0900

    p

--- DFS-BFS/boj22944.py
+++ DFS-BFS/boj22944.py
@@ -1,60 +1,67 @@
+#맨해탄 거리 ??
+
 from collections import deque
 import sys
 
 input = sys.stdin.readline
 
 
-N, H, D = map(int,input().split())
-# 한변 길이, 체력, 우산내구도
+N, H, D = map(int, input().split())
 
-#우산 사용 시작떄부터 내구도 감소
-
-#u우산 현재s 도착 e 빈칸 
-visited = [[0]*N for _ in range(N)]
 matrix = []
 for _ in range(N):
     matrix.append(input().rstrip())
+# 각 좌표별로 거리 구하고 성공할수있는지 찾기
 
-dx = [-1,0,0,1]
-dy = [0,-1,1,0]
+cord = []
 
-def bfs(a,b):
-    global H
-    q = deque()
-    q.append((a,b))
-    visited[a][b] = 1
-    while q:
-        H -= 1
-        for _ in range(len(q)):
-            x,y = q.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-
-                if 0 <= nx < N and 0 <= ny < N:
-                    if matrix[nx][ny] == 'E':
-                        return visited[x][y]
-                    if visited[nx][ny] == 0:
-                        if matrix[nx][ny] == 'U':
-                            H += D
-                            visited[nx][ny] = visited[x][y] + 1
-                            q.append((nx,ny))
-                        else:
-                            visited[nx][ny] = visited[x][y] + 1
-                            q.append((nx,ny))
-                    elif visited[nx][ny] < visited[x][y]:
-                        visited[nx][ny] = visited[x][y] + 1
-                        q.append((nx,ny))
-        if H == 0:
-            return -1
-        
-        for i in visited:
-            print(i)
-        print()
-        
-
+#매트릭스 돌면서 좌표 수집
 for i in range(N):
     for j in range(N):
-        if matrix[i][j] == 'S':
-            print(bfs(i,j))
-            break
+        cursor = matrix[i][j]
+
+        if cursor == 'S':
+            start = (i,j)
+        elif cursor != '.':
+            cord.append((i,j))
+            if cursor == 'E':
+                end = (i,j)
+
+visited = [[0,0]for _ in range(len(cord))]
+
+x, y = start
+q = deque()
+q.append((x,y,H,0,0))
+count = 0
+print('end',end)
+result = []
+while q:
+    print(q)
+    x,y,health,umb,cnt = q.popleft()
+    #health 현재 체력
+    #umb 현재 우산 내구도
+    #cnt 지금까지의 이동 횟수
+    for i in range(len(cord)):
+        #각 지점과 현재위치 사이의 맨해탄 거리
+            mattdist = abs(x-cord[i][0])+abs(y-cord[i][1])
+            print(cnt,x,y,cord[i],mattdist)
+            if health + umb >= mattdist:
+                next_health = health
+                if umb == 0:
+                    next_health = health - mattdist +1
+                elif umb - mattdist < 0:
+                    next_health = health + umb - mattdist+1
+                    print(health,umb,mattdist,next_health,cord[i])
+                if cord[i] == end:  # E 탐색 성공
+                    result.append(cnt+mattdist)
+                    continue
+                if next_health <= 0:    # 체력이 0 이하로 떨어지면 ㅃ
+                    continue
+                if visited[i][0] < next_health:    # 이전 방문보다 높은 체력으로 방문해서 더 멀리갈 수 있다면 append
+                    q.append((cord[i][0],cord[i][1], next_health , D-1 , cnt+mattdist))
+                    visited[i][0] = next_health
+if result:
+    #최소 이동으로 E에 방문할때를 출력
+    print(min(result))
+else:
+    print(-1) # 탐색 실패
