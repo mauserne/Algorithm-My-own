775f2b8ae1e201b9f6df313fd985cabc79e8809f
mauserne <52377363+mauserne@users.noreply.github.com>
2022-06-13 23:34:38 +0900

    p

--- NOTE.py
+++ NOTE.py
@@ -1,1 +1,17 @@
-from collections import Counter
+N, B = input().split()
+
+N = N[::-1]
+
+result = 0
+
+for i in range(len(N)):
+    if N[i].isalpha():
+        print(N[i],'ispalpha')
+        result += (ord(N[i]) - 55) * int(B) **i
+    else:
+        print(N[i],'is digit')
+        result += int(N[i]) * int(B) **i
+
+print(result)
+
+print('1'.isalpha)
